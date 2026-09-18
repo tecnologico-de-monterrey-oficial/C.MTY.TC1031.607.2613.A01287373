6a6da3469a16d2cad74eae0456109490b645688f
Carolina <a01287373@tec.mx>
2026-09-18 13:14:09 -0600

    1

---
+++ Evidences/Evidence1/Evidencia1.cpp
@@ -0,0 +1,25 @@
+
+// Carolina Vildosola Guzman
+// A01287373
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//  Log representa una linea del archivo
+struct Log {
+    string mes;
+    int dia;
+    int anio;
+    string hora;
+    string ip;
+    string mensaje;
+};
+
+int main() {
+
+    return 0;
+}
