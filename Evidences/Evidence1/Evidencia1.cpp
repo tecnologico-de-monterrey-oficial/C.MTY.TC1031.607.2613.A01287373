1f65f7db499d9c073b83838bb54ec89a16e0e139
Carolina <a01287373@tec.mx>
2026-09-19 17:41:10 -0600

    2

--- Evidences/Evidence1/Evidencia1.cpp
+++ Evidences/Evidence1/Evidencia1.cpp
@@ -9,7 +9,9 @@
 
 using namespace std;
 
-//  Log representa una linea del archivo
+// Un log es un registro de algo que paso en el sistema 
+// Cada linea del archivo es un log y guarda la fecha, hora, IP y lo que paso
+// Hacemos este struct para guardar juntos todos esos datos de cada log
 struct Log {
     string mes;
     int dia;
